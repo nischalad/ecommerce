ad5b405fcc0c33aefbf3ccd7c40fb7baae296258
nischalad <nischaladhikari2034@gmail.com>
2025-05-18 23:16:02 +0545

    Initial commit - setup navbar and responsive menu

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+        {/* Hero Left Side */}
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
